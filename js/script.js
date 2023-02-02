48a9b00ccd99fa0ddd7ef054efec5bfe6a2d3a9a
Madu-Montes <monteseduarda56@gmail.com>
2023-02-02 16:50:48 -0300

    initial

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var filmeDuda  = 'https://br.web.img3.acsta.net/pictures/210/527/21052756_20131024195513383.jpg';
+
+var filmeMadu = 'https://images.justwatch.com/poster/265228426/s718/o-sussuro-do-coracao.%7Bformat%7D';
+
+var filmeMaria = 'https://m.media-amazon.com/images/I/81zeqn6hofL.jpg';
